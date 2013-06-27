c964b14aaca3b5b72b8d5759b1ca34f032f002de
Dmitriy Likhten <dlikhten@gmail.com>
2013-06-27 00:16:48 -0400

    Update backbone-filtered-collection.js
    Adding a version number to the javascript, so people can at least track what is going on.

--- backbone-filtered-collection.js
+++ backbone-filtered-collection.js
@@ -19,6 +19,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+/* version 1.0.0 */
 (function(_, Backbone) {
   var defaultFilter = function() {return true;};
   /**
