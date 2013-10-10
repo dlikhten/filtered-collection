be9d86707a8a87026b86827c8978b7bcf0806f80
Dmitriy Likhten <dlikhten@gmail.com>
2013-10-10 12:04:55 -0400

    Version bump

--- vendor/assets/javascripts/backbone-filtered-collection.js
+++ vendor/assets/javascripts/backbone-filtered-collection.js
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-/* version 1.1.0 */
+/* version 1.2.0 */
 (function(_, Backbone) {
   var defaultFilter = function() {return true;};
   /**
