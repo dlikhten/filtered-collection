62432bec8b5512041b3f867a88c47b0c1be9d301
Dmitriy Likhten <dlikhten@gmail.com>
2013-10-10 10:20:57 -0400

    clean up specs

--- spec/javascripts/backbone-filtered-collection-spec.js
+++ spec/javascripts/backbone-filtered-collection-spec.js
@@ -16,12 +16,9 @@ describe("Backbone.FilteredCollection", function() {
     }
   };
 
-  var oddFilter = function(model) {
-    return model.get("value") % 2 == 1;
-  }
   var evenFilter = function(model) {
     return model.get("value") % 2 == 0;
-  }
+  };
 
   beforeEach(function() {
     allModels = new RegularModelCollection();
@@ -37,16 +34,15 @@ describe("Backbone.FilteredCollection", function() {
   });
 
   describe("#setFilter", function() {
-    it("should filter the given model", function() {
+    it("filters the given model", function() {
       collection.setFilter(createLessthanFilter(5));
 
       expect(collection.length).toEqual(5);
       expect(collection.at(0).get('value')).toEqual(0);
     });
 
-    it("should change filters", function() {
+    it("uses the given filter", function() {
       collection.setFilter(createLessthanFilter(5));
-
       collection.setFilter(function(model) {
         return model.get('value') > 7;
       });
@@ -55,7 +51,7 @@ describe("Backbone.FilteredCollection", function() {
       expect(collection.at(0).get('value')).toEqual(8);
     });
 
-    it("should take a false filter as a return to no filter", function() {
+    it("accepts false to remove all filters", function() {
       collection.setFilter(createLessthanFilter(5));
       expect(collection.length).toEqual(5);
       collection.setFilter(undefined); // no change
@@ -66,46 +62,43 @@ describe("Backbone.FilteredCollection", function() {
       expect(collection.length).toEqual(10);
     });
 
-    it("should work correctly after filtering is changed constantly", function() {
+    it("works correctly after filtering is changed constantly", function() {
       collection.setFilter(createLessthanFilter(0));
       expect(collection.models.length).toEqual(0);
 
       collection.setFilter(createLessthanFilter(3));
       expect(collection.models.length).toEqual(3);
-      expect(collection.models[0].get("value")).toEqual(0)
-      expect(collection.models[1].get("value")).toEqual(1)
-      expect(collection.models[2].get("value")).toEqual(2)
+      expect(collection.at(0).get("value")).toEqual(0);
+      expect(collection.at(1).get("value")).toEqual(1);
+      expect(collection.at(2).get("value")).toEqual(2);
 
       collection.setFilter(evenFilter);
       expect(collection.models.length).toEqual(5);
-      expect(collection.models[0].get("value")).toEqual(0)
-      expect(collection.models[1].get("value")).toEqual(2)
-      expect(collection.models[2].get("value")).toEqual(4)
-      expect(collection.models[3].get("value")).toEqual(6)
-      expect(collection.models[4].get("value")).toEqual(8)
+      expect(collection.at(0).get("value")).toEqual(0);
+      expect(collection.at(1).get("value")).toEqual(2);
+      expect(collection.at(2).get("value")).toEqual(4);
+      expect(collection.at(3).get("value")).toEqual(6);
+      expect(collection.at(4).get("value")).toEqual(8);
     });
 
-    it("should not trigger a filter-complete event if options.silent is true", function() {
-      count = 0;
-      collection.on("filter-complete", function() {
-        count += 1;
-      });
+    it("does not trigger a filter-complete event if options.silent is true", function() {
+      var eventSpy = jasmine.createSpy('filter-complete listener');
+      collection.on("filter-complete", eventSpy);
 
       collection.setFilter(createLessthanFilter(0), {silent: true});
-
-      expect(count).toEqual(0);
+      expect(eventSpy).not.toHaveBeenCalled();
     });
   });
 
   describe("event:add", function() {
-    it("should not add the new object, since it is already filtered out", function() {
+    it("does not add an already filtered out new object", function() {
       collection.setFilter(createLessthanFilter(5));
       expect(collection.length).toEqual(5);
       allModels.add(new TehModel({value: 6}));
       expect(collection.length).toEqual(5);
     });
 
-    it("should add the new object, since it passes the filter", function() {
+    it("adds the new object, since it passes the filter", function() {
       collection.setFilter(createLessthanFilter(5));
       expect(collection.length).toEqual(5);
       allModels.add(new TehModel({value: 1}));
@@ -113,7 +106,7 @@ describe("Backbone.FilteredCollection", function() {
       expect(collection.at(5).get('value')).toEqual(1);
     });
 
-    it("should add the new object to the correct location", function() {
+    it("adds the new object to the correct location", function() {
       collection.setFilter(createLessthanFilter(5));
       expect(collection.length).toEqual(5);
       allModels.add(new TehModel({value: 4}), {at: 0});
@@ -121,23 +114,18 @@ describe("Backbone.FilteredCollection", function() {
       expect(collection.at(0).get('value')).toEqual(4);
     });
 
-    it("should trigger an add event if the object was added", function() {
+    it("triggers an add event if the object was added", function() {
       collection.setFilter(createLessthanFilter(5));
-      expect(collection.length).toEqual(5);
-
       var newModel = new TehModel({value: 3});
-      count = 0;
-      collection.on("add", function(model, collection, options) {
-        expect(model).toEqual(newModel);
-        expect(options.index).toEqual(0);
-        count += 1;
-      });
+      var eventSpy = jasmine.createSpy('reset event listener');
+
+      collection.on("add", eventSpy);
       allModels.add(newModel, {at: 0});
 
-      expect(count).toEqual(1);
+      expect(eventSpy).toHaveBeenCalledWith(newModel, collection, {index: 0})
     });
 
-    it("should re-number elements propperly in the mapping according to what the actualy indices are in the original collection", function() {
+    it("re-numbers elements properly in the mapping according to what the atual indices are in the original collection", function() {
       collection.setFilter(createLessthanFilter(10));
       expect(collection.length).toEqual(10);
 
@@ -148,14 +136,14 @@ describe("Backbone.FilteredCollection", function() {
   });
 
   describe("event:remove", function() {
-    it("should be a noop since the object is filtered", function() {
+    it("is a no-op if the item removed is already not filtered", function() {
       collection.setFilter(createLessthanFilter(5));
       expect(collection.length).toEqual(5);
       allModels.remove(allModels.at(6));
       expect(collection.length).toEqual(5);
     });
 
-    it("should be a remove the removed object", function() {
+    it("removes the model that was removed from the underlying collection", function() {
       collection.setFilter(createLessthanFilter(5));
       expect(collection.length).toEqual(5);
       allModels.remove(allModels.at(4));
@@ -163,7 +151,7 @@ describe("Backbone.FilteredCollection", function() {
       expect(collection.at(collection.length - 1).get('value')).toEqual(3);
     });
 
-    it("should re-number elements propperly in the mapping according to what the actualy indices are in the original collection", function() {
+    it("should re-number elements properly in the mapping according to what the actual indices are in the original collection", function() {
       collection.setFilter(createLessthanFilter(10));
       expect(collection.length).toEqual(10);
 
@@ -174,7 +162,7 @@ describe("Backbone.FilteredCollection", function() {
   });
 
   describe("event:reset", function() {
-    it("should be a noop since the object is filtered", function() {
+    it("resets all models to those in the underlying collection", function() {
       collection.setFilter(createLessthanFilter(15));
       var newAll = [];
       for (var i = 10; i < 20; i++) {
@@ -184,10 +172,20 @@ describe("Backbone.FilteredCollection", function() {
       expect(collection.length).toEqual(5);
       expect(collection.at(4).get('value')).toEqual(14);
     });
+
+    it("triggers exactly one reset event", function() {
+      var eventSpy = jasmine.createSpy('reset event listener');
+      collection.on('reset', eventSpy);
+
+      allModels.reset([{id: 11, value: 11}]);
+
+      expect(eventSpy).toHaveBeenCalledWith(collection);
+      expect(eventSpy.callCount).toEqual(1);
+    });
   });
 
   describe("event:sort", function() {
-    it("should continue filtering the collection, except with a new order", function() {
+    it("continues to filter the collection, except with a new order", function() {
       collection.setFilter(createLessthanFilter(5));
       allModels.comparator = function(v1, v2) {
         return v2.get("value") - v1.get("value");
@@ -204,25 +202,26 @@ describe("Backbone.FilteredCollection", function() {
   });
 
   describe("event:filter-complete", function() {
-    it("should fire when the underlying collection fires it (thus we're done filtering too)", function() {
-      var filterFired = 0;
-      collection.on("filter-complete", function() {
-        filterFired += 1;
-      });
+    it("triggers when the underlying collection triggers it (thus we're done filtering too)", function() {
+      var eventSpy = jasmine.createSpy('filter-complete event listener');
+      collection.on("filter-complete", eventSpy);
+
       allModels.trigger("filter-complete");
-      expect(filterFired).toEqual(1);
+
+      expect(eventSpy).toHaveBeenCalled();
     });
 
-    it("should fire once only at the end of a filter", function() {
-      var filterFired = 0;
-      collection.on("filter-complete", function() {
-        filterFired += 1;
-      });
+    it("triggers once only at the end of a filter", function() {
+      var eventSpy = jasmine.createSpy('filter-complete event listener');
+      collection.on("filter-complete", eventSpy);
+
       collection.setFilter(createLessthanFilter(3));
-      expect(filterFired).toEqual(1);
+
+      expect(eventSpy).toHaveBeenCalled();
+      expect(eventSpy.callCount).toEqual(1);
     });
 
-    it("should fire once when a change is propagated from an underlying model", function() {
+    it("triggers once when a change is propagated from an underlying model", function() {
       var filterFired = 0;
       collection.on("filter-complete", function() {
         filterFired += 1;
@@ -230,51 +229,51 @@ describe("Backbone.FilteredCollection", function() {
       collection.setFilter(createLessthanFilter(3));
       filterFired = 0;
 
-      collection.models[0].trigger("change", collection.models[0], allModels)
+      collection.at(0).trigger("change", collection.at(0), allModels)
       expect(filterFired).toEqual(1);
     });
   });
 
   describe("model - event:destroy", function() {
-    it("should just remove the model from the base collection like normal, and raise no problems with the filter", function() {
+    it("just removes the model from the base collection like normal, and raise no problems with the filter", function() {
       collection.setFilter(createLessthanFilter(5));
-      origModelZero = collection.models[0];
+      origModelZero = collection.at(0);
       // simulate an ajax destroy
-      origModelZero.trigger("destroy", origModelZero, origModelZero.collection)
+      origModelZero.trigger("destroy", origModelZero, origModelZero.collection);
 
-      expect(collection.models[0].get("value")).toEqual(1)
+      expect(collection.at(0).get("value")).toEqual(1);
     });
 
-    it("should remove elements from the model as events occur", function() {
+    it("removes elements from the model as events occur", function() {
       collection.setFilter(createLessthanFilter(10));
 
       // start removing in weird orders, make sure vents are done properly
-      model = collection.models[0];
-      model.trigger("destroy", model, model.collection)
-      expect(collection.models[0].get("value")).toEqual(1)
+      model = collection.at(0);
+      model.trigger("destroy", model, model.collection);
+      expect(collection.at(0).get("value")).toEqual(1);
 
-      model = collection.models[3];
-      model.trigger("destroy", model, model.collection)
-      expect(collection.models[3].get("value")).toEqual(5)
+      model = collection.at(3);
+      model.trigger("destroy", model, model.collection);
+      expect(collection.at(3).get("value")).toEqual(5);
 
-      model = collection.models[3];
-      model.trigger("destroy", model, model.collection)
-      expect(collection.models[3].get("value")).toEqual(6)
+      model = collection.at(3);
+      model.trigger("destroy", model, model.collection);
+      expect(collection.at(3).get("value")).toEqual(6);
 
-      model = collection.models[3];
-      model.trigger("destroy", model, model.collection)
-      expect(collection.models[3].get("value")).toEqual(7)
+      model = collection.at(3);
+      model.trigger("destroy", model, model.collection);
+      expect(collection.at(3).get("value")).toEqual(7);
 
-      model = collection.models[2];
-      model.trigger("destroy", model, model.collection)
-      expect(collection.models[2].get("value")).toEqual(7)
+      model = collection.at(2);
+      model.trigger("destroy", model, model.collection);
+      expect(collection.at(2).get("value")).toEqual(7);
 
-      model = collection.models[1];
-      model.trigger("destroy", model, model.collection)
-      expect(collection.models[1].get("value")).toEqual(7)
+      model = collection.at(1);
+      model.trigger("destroy", model, model.collection);
+      expect(collection.at(1).get("value")).toEqual(7);
     });
 
-    it("should create remove events for every deleted model", function() {
+    it("triggers remove events for every deleted model", function() {
       collection.setFilter(createLessthanFilter(10));
       var lastModelRemoved = null;
       var count = 0;
@@ -285,37 +284,37 @@ describe("Backbone.FilteredCollection", function() {
 
       // start removing in weird orders, make sure vents are done properly
       count = 0;
-      model = collection.models[0];
+      model = collection.at(0);
       model.trigger("destroy", model, model.collection)
       expect(lastModelRemoved).toEqual(model);
       expect(count).toEqual(1);
 
       count = 0;
-      model = collection.models[3];
+      model = collection.at(3);
       model.trigger("destroy", model, model.collection)
       expect(lastModelRemoved).toEqual(model);
       expect(count).toEqual(1);
 
       count = 0;
-      model = collection.models[3];
+      model = collection.at(3);
       model.trigger("destroy", model, model.collection)
       expect(lastModelRemoved).toEqual(model);
       expect(count).toEqual(1);
 
       count = 0;
-      model = collection.models[3];
+      model = collection.at(3);
       model.trigger("destroy", model, model.collection)
       expect(lastModelRemoved).toEqual(model);
       expect(count).toEqual(1);
 
       count = 0;
-      model = collection.models[2];
+      model = collection.at(2);
       model.trigger("destroy", model, model.collection)
       expect(lastModelRemoved).toEqual(model);
       expect(count).toEqual(1);
 
       count = 0;
-      model = collection.models[1];
+      model = collection.at(1);
       model.trigger("destroy", model, model.collection)
       expect(lastModelRemoved).toEqual(model);
       expect(count).toEqual(1);
@@ -331,17 +330,17 @@ describe("Backbone.FilteredCollection", function() {
       removeSpy = jasmine.createSpy("remove listener");
     });
 
-    it("should remove the model because it failed the filter post change, triggers remove event", function() {
+    it("removes the model because it failed the filter post change, triggers remove event", function() {
       collection.setFilter(createLessthanFilter(5));
       collection.on("change", changeSpy);
       collection.on("add", addSpy);
       collection.on("remove", removeSpy);
 
-      origModelZero = collection.models[0];
+      origModelZero = collection.at(0);
       origModelZero.set("value", 10)
 
       expect(collection.models.length).toEqual(4)
-      expect(collection.models[0].get("value")).toEqual(1)
+      expect(collection.at(0).get("value")).toEqual(1)
       expect(changeSpy).not.toHaveBeenCalled();
       expect(addSpy).not.toHaveBeenCalled();
       expect(removeSpy).toHaveBeenCalledWith(origModelZero, collection, jasmine.any(Object));
@@ -353,27 +352,27 @@ describe("Backbone.FilteredCollection", function() {
       collection.on("add", addSpy);
       collection.on("remove", removeSpy);
 
-      origModelZero = collection.models[0];
+      origModelZero = collection.at(0);
       origModelZero.set("value", 3)
 
       expect(collection.models.length).toEqual(5)
-      expect(collection.models[0].get("value")).toEqual(3)
+      expect(collection.at(0).get("value")).toEqual(3)
       expect(changeSpy).toHaveBeenCalledWith(origModelZero, collection);
       expect(addSpy).not.toHaveBeenCalled();
       expect(removeSpy).not.toHaveBeenCalled();
     });
 
-    it("should add the model that is now passing the filter, triggers add event", function() {
+    it("adds the model that is now passing the filter, triggers add event", function() {
       collection.setFilter(createLessthanFilter(5));
       collection.on("change", changeSpy);
       collection.on("add", addSpy);
       collection.on("remove", removeSpy);
 
-      origModelZero = allModels.models[9];
+      origModelZero = allModels.at(9);
       origModelZero.set("value", 2)
 
       expect(collection.models.length).toEqual(6)
-      expect(collection.models[5].get("value")).toEqual(2)
+      expect(collection.at(5).get("value")).toEqual(2)
       expect(changeSpy).not.toHaveBeenCalled();
       expect(addSpy).toHaveBeenCalledWith(origModelZero, collection, jasmine.any(Object));
       expect(removeSpy).not.toHaveBeenCalled();
@@ -444,7 +443,7 @@ describe("Backbone.FilteredCollection", function() {
       collection.on('all', eventSpy);
 
       allModels.add(new TehModel({id: 16, value: 16}));
-      allModels.remove(allModels.models[0]);
+      allModels.remove(allModels.at(0));
       allModels.get(2).set("value", "3");
       allModels.comparator = function(a, b) { return 0 };
       allModels.sort();
